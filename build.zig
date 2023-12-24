0b809436e0716bbfd24e3f91e2e5819a643d23c1
viri <hi@viri.sh>
2023-12-24 16:58:00 -0600

    zig build: add zigwin32 module dependency

--- build.zig
+++ build.zig
@@ -1,8 +1,12 @@
+const builtin = @import("builtin");
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const wth = b.addModule("wth", .{
-        .dependencies = &.{},
+        .dependencies = if (builtin.target.os.tag == .windows)
+            &.{.{ .name = "zigwin32", .module = b.dependency("zigwin32", .{}).module("zigwin32") }}
+        else
+            &.{},
         .source_file = std.Build.LazyPath.relative("src/wth.zig"),
     });
 
