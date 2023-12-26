dd1cd8afb2f907fc7a74f3f8c6b1d970eca72465
viri <hi@viri.sh>
2023-12-26 01:05:41 -0600

    win32: create message fiber

--- src/win32/impl.zig
+++ src/win32/impl.zig
@@ -18,13 +18,25 @@ const CP_UTF8                                    = zigwin32.globalization.CP_UTF
 const MB_PRECOMPOSED                             = zigwin32.globalization.MB_PRECOMPOSED;
 const MultiByteToWideChar                        = zigwin32.globalization.MultiByteToWideChar;
 const IMAGE_DOS_HEADER                           = zigwin32.system.system_services.IMAGE_DOS_HEADER;
+const CreateFiber                                = zigwin32.system.threading.CreateFiber;
+const DeleteFiber                                = zigwin32.system.threading.DeleteFiber;
+const ConvertFiberToThread                       = zigwin32.system.threading.ConvertFiberToThread;
+const ConvertThreadToFiber                       = zigwin32.system.threading.ConvertThreadToFiber;
+const SwitchToFiber                              = zigwin32.system.threading.SwitchToFiber;
 const AdjustWindowRectExForDpi                   = zigwin32.ui.hi_dpi.AdjustWindowRectExForDpi;
 const EnableNonClientDpiScaling                  = zigwin32.ui.hi_dpi.EnableNonClientDpiScaling;
 const DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE    = zigwin32.ui.hi_dpi.DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE;
 const DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2 = zigwin32.ui.hi_dpi.DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2;
 const SetThreadDpiAwarenessContext               = zigwin32.ui.hi_dpi.SetThreadDpiAwarenessContext;
+const CreateWindowExW                            = zigwin32.ui.windows_and_messaging.CreateWindowExW;
 const DefWindowProcW                             = zigwin32.ui.windows_and_messaging.DefWindowProcW;
+const DestroyWindow                              = zigwin32.ui.windows_and_messaging.DestroyWindow;
+const DispatchMessageW                           = zigwin32.ui.windows_and_messaging.DispatchMessageW;
+const MSG                                        = zigwin32.ui.windows_and_messaging.MSG;
+const PeekMessageW                               = zigwin32.ui.windows_and_messaging.PeekMessageW;
+const PM_REMOVE                                  = zigwin32.ui.windows_and_messaging.PM_REMOVE;
 const RegisterClassExW                           = zigwin32.ui.windows_and_messaging.RegisterClassExW;
+const TranslateMessage                           = zigwin32.ui.windows_and_messaging.TranslateMessage;
 const UnregisterClassW                           = zigwin32.ui.windows_and_messaging.UnregisterClassW;
 const WNDCLASSEXW                                = zigwin32.ui.windows_and_messaging.WNDCLASSEXW;
 // zig fmt: on
@@ -34,7 +46,9 @@ extern "ntdll" fn RtlGetNtVersionNumbers(*u32, *u32, *u32) callconv(WINAPI) void
 
 const global = struct {
     var class_atom: u16 = undefined;
+    var main_fiber: *anyopaque = undefined;
     var main_thread_id: u32 = undefined;
+    var message_fiber: *anyopaque = undefined;
 
     // Windows 10 1703 (Build 15063; April 2017; Redstone / "Creators Update")
     var win10_1703_or_later: bool = undefined;
@@ -118,6 +132,17 @@ pub fn init(_: wth.InitOptions) wth.InitError!void {
     if (global.class_atom == 0) {
         return error.SystemResources;
     }
+    errdefer assert(UnregisterClassW(class_name.ptr, imageBase()) != 0);
+
+    // TODO: I think there's many reasons why this can happen. Investigate.
+    global.main_fiber = ConvertThreadToFiber(null) orelse
+        return error.SystemResources;
+    errdefer assert(ConvertFiberToThread() != 0);
+
+    // TODO: same as above i didnt actually read the errors
+    global.message_fiber = CreateFiber(fiber_proc_stack_size, fiberProc, null) orelse
+        return error.SystemResources;
+    errdefer DeleteFiber(global.message_fiber);
 
     // store this for later (probably only useful for debug checks)
     global.main_thread_id = getCurrentThreadId();
@@ -129,6 +154,21 @@ pub fn deinit() void {
         break :blk @ptrFromInt(global.class_atom);
     };
     assert(UnregisterClassW(class_atom, imageBase()) != 0);
+    assert(ConvertFiberToThread() != 0);
+    DeleteFiber(global.message_fiber);
+}
+
+const fiber_proc_stack_size = 2048;
+fn fiberProc(_: ?*anyopaque) callconv(WINAPI) void {
+    var msg: MSG = undefined;
+    while (true) {
+        while (PeekMessageW(&msg, null, 0, 0, PM_REMOVE) != 0) {
+            // TODO: gate translatemessage on wanting actual-input
+            _ = TranslateMessage(&msg);
+            _ = DispatchMessageW(&msg);
+        }
+        SwitchToFiber(global.main_fiber);
+    }
 }
 
 fn windowProc(
