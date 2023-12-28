9ac118387fb11d94ee045940a1e04c200bb1b828
viri <hi@viri.sh>
2023-12-28 01:12:35 -0600

    zig build: remove zigwin32 dependency

--- build.zig
+++ build.zig
@@ -3,10 +3,7 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const wth = b.addModule("wth", .{
-        .dependencies = if (builtin.target.os.tag == .windows)
-            &.{.{ .name = "zigwin32", .module = b.dependency("zigwin32", .{}).module("zigwin32") }}
-        else
-            &.{},
+        .dependencies = &.{},
         .source_file = std.Build.LazyPath.relative("src/wth.zig"),
     });
 
