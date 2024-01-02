d275f62e565482c77201baab7333516879587560
viri <hi@viri.sh>
2024-01-02 01:58:11 -0600

    wth: add Event and stuff

---
+++ src/__flags.zig
@@ -0,0 +1,3 @@
+pub const multi_window: bool = true;
+pub const text_input: bool = false;
+pub const win32_fibers: bool = true;
--- src/wth.zig
+++ src/wth.zig
@@ -1,27 +1,44 @@
 const builtin = @import("builtin");
 const std = @import("std");
 
+const __flags = @import("__flags.zig");
+
 const impl = switch (builtin.target.os.tag) {
     .windows => @import("win32/impl.zig"),
     else => @compileError("unsupported platform"),
 };
 
-pub const InitError = error{SystemResources};
+pub const InitError = std.mem.Allocator.Error || error{SystemResources};
 pub const InitOptions = struct {};
 
-pub fn init(options: InitOptions) InitError!void {
-    return try impl.init(options);
+pub fn init(allocator: std.mem.Allocator, options: InitOptions) InitError!void {
+    return try impl.init(allocator, options);
 }
 
 pub fn deinit() void {
     impl.deinit();
 }
 
-pub const SyncError = error{Shutdown};
+pub fn events() []const Event {
+    return impl.events();
+}
+
+pub const SyncError = std.mem.Allocator.Error || error{Shutdown};
 
 pub fn sync() SyncError!void {
     return try impl.sync();
 }
+
+pub const Event = union(enum) {
+    close_request: Window.Tag,
+    mouse_move: MouseMove,
+
+    pub const MouseMove = struct {
+        x: Window.Coordinate,
+        y: Window.Coordinate,
+        window: Window.Tag,
+    };
+};
 
 pub const Window = struct {
     impl: impl.Window,
@@ -30,21 +47,38 @@ pub const Window = struct {
     pub const CreateOptions = struct {
         controls: WindowControls = .{},
         class_name: []const u8 = "wth",
-        size: [2]u15 = .{ 800, 608 },
+        size: [2]Window.Coordinate = .{ 800, 608 },
         title: []const u8 = "a nice window",
     };
+    pub const Coordinate = u15;
+    pub const Tag = struct {
+        impl: if (__flags.multi_window) impl.Window.Tag else void,
 
-    pub fn create(allocator: std.mem.Allocator, options: CreateOptions) CreateError!*Window {
+        pub fn format(_: Tag, comptime _: []const u8, _: std.fmt.FormatOptions, writer: anytype) !void {
+            try writer.writeAll("(opaque)");
+        }
+
+        pub fn equal(value: Tag, other_tag: Tag) bool {
+            return value.impl.equal(other_tag.impl);
+        }
+    };
+
+    pub fn create(options: CreateOptions) CreateError!*Window {
+        const allocator = impl.getAllocator();
         const window = try allocator.create(Window);
         errdefer allocator.destroy(window);
-        try window.impl.emplace(allocator, options);
+        try window.impl.emplace(options);
         return window;
     }
 
     pub fn destroy(window: *Window) void {
-        const allocator = window.impl.allocator;
+        const allocator = impl.getAllocator();
         defer allocator.destroy(window);
         window.impl.deinit();
+    }
+
+    pub fn tag(window: *Window) Window.Tag {
+        return window.impl.tag();
     }
 };
 
